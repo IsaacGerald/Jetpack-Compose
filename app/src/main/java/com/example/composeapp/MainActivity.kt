e89a0acbb1ff065577ff8d614593d068e64d2a81
Isaac Gerald <odhiambo.gerald@eclectics.io>
2023-04-08 10:35:55 +0300

    creating card using jectpack compose

---
+++ app/src/main/java/com/example/composeapp/MainActivity.kt
@@ -0,0 +1,87 @@
+package com.example.composeapp
+
+import android.os.Bundle
+import android.os.ParcelFileDescriptor
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Paint
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.composeapp.ui.theme.ComposeAppTheme
+import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            val painter = painterResource(id = R.drawable.free_winter)
+            val title = "Kermit is playing in the snow"
+            val contentDescription = "Kermit is playing in the snow"
+            Box(modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth(0.5f)) {
+                ImageCard(painter = painter, title = title, contentDescription = contentDescription)
+            }
+        }
+    }
+}
+
+@Composable
+fun ImageCard(
+    painter: Painter,
+    title: String,
+    contentDescription: String,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier.fillMaxWidth(),
+        shape = RoundedCornerShape(15.dp),
+        elevation = 5.dp
+    ) {
+        Box(modifier = modifier.height(200.dp)) {
+            Image(
+                painter = painter,
+                contentDescription = contentDescription,
+                contentScale = ContentScale.Crop
+            )
+            Box(modifier = Modifier
+                .fillMaxSize()
+                .background(brush = Brush.verticalGradient(
+                    colors = listOf(
+                        Color.Transparent, Color.Black
+                    ),
+                    startY = 300f
+                ))) {
+
+            }
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(12.dp),
+                contentAlignment = Alignment.BottomStart
+            ) {
+                Text(text = title, style = TextStyle(color = Color.White), fontSize = 16.sp)
+            }
+        }
+    }
+}
