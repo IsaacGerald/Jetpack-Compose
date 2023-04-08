e46d766b0ef5e19fdf05e36ae2922db0219fa578
Isaac Gerald <odhiambo.gerald@eclectics.io>
2023-04-08 11:20:14 +0300

    styling text

--- app/src/main/java/com/example/composeapp/MainActivity.kt
+++ app/src/main/java/com/example/composeapp/MainActivity.kt
@@ -23,7 +23,16 @@ import androidx.compose.ui.graphics.Paint
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -33,55 +42,57 @@ import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val fontFamily = FontFamily(
+            Font(R.font.lexend_thin, FontWeight.Thin),
+            Font(R.font.lexend_black, FontWeight.Black),
+            Font(R.font.lexend_bold, FontWeight.Bold),
+            Font(R.font.lexend_extrabold, FontWeight.ExtraBold),
+            Font(R.font.lexend_light, FontWeight.Light),
+            Font(R.font.lexend_extralight, FontWeight.ExtraLight),
+            Font(R.font.lexend_medium, FontWeight.Medium),
+            Font(R.font.lexend_regular, FontWeight.Normal),
+            Font(R.font.lexend_semibold, FontWeight.SemiBold),
+        )
+
         setContent {
-            val painter = painterResource(id = R.drawable.free_winter)
-            val title = "Kermit is playing in the snow"
-            val contentDescription = "Kermit is playing in the snow"
-            Box(modifier = Modifier
-                .padding(16.dp)
-                .fillMaxWidth(0.5f)) {
-                ImageCard(painter = painter, title = title, contentDescription = contentDescription)
-            }
-        }
-    }
-}
-
-@Composable
-fun ImageCard(
-    painter: Painter,
-    title: String,
-    contentDescription: String,
-    modifier: Modifier = Modifier
-) {
-    Card(
-        modifier = modifier.fillMaxWidth(),
-        shape = RoundedCornerShape(15.dp),
-        elevation = 5.dp
-    ) {
-        Box(modifier = modifier.height(200.dp)) {
-            Image(
-                painter = painter,
-                contentDescription = contentDescription,
-                contentScale = ContentScale.Crop
-            )
-            Box(modifier = Modifier
-                .fillMaxSize()
-                .background(brush = Brush.verticalGradient(
-                    colors = listOf(
-                        Color.Transparent, Color.Black
-                    ),
-                    startY = 300f
-                ))) {
-
-            }
             Box(
                 modifier = Modifier
                     .fillMaxSize()
-                    .padding(12.dp),
-                contentAlignment = Alignment.BottomStart
+                    .background(Color(0xFF101010))
             ) {
-                Text(text = title, style = TextStyle(color = Color.White), fontSize = 16.sp)
+                Text(
+                    text = buildAnnotatedString {
+                                                withStyle(
+                                                    style = SpanStyle(
+                                                        color = Color.Green,
+                                                        fontSize = 50.sp
+                                                    )
+                                                ){
+                                                    append("J")
+                                                }
+                        append("etpack ")
+                        withStyle(
+                            style = SpanStyle(
+                                color = Color.Green,
+                                fontSize = 50.sp
+                            )
+                        ){
+                            append("C")
+                        }
+                        append("ompose ")
+                    },
+                    color = Color.White,
+                    fontSize = 30.sp,
+                    fontFamily = fontFamily,
+                    fontWeight = FontWeight.Bold,
+                    fontStyle = FontStyle.Italic,
+                    textAlign = TextAlign.Center,
+                    textDecoration = TextDecoration.Underline
+                )
             }
         }
     }
 }
+
+
